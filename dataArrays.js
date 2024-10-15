062d2318d3dd2cb77b3e086d407069e667046828
Mieszko Czubiński <mieszkoczubinski@gmail.com>
2024-10-15 13:47:55 +0200

    dopłaty, etc

---
+++ dataArrays.js
@@ -0,0 +1,59 @@
+const revenues = [
+    "zakładany plon",
+    "plon z kontraktacji",
+    "plon pozakontraktowy",
+    "ilość wysłodków (10% s.m) na sprzedaż",
+    "cena buraków kontraktowych",
+    "cena buraków pozakontraktowych",
+    "cena wysłodków"
+  ];
+  const revenuesUnits = [
+    "t/ha",
+    "t/ha",
+    "t/ha",
+    "t/ha",
+    "zł/t",
+    "zł/t",
+    "zł/t"
+  ];
+  const revenuesValues = [
+    70.00,
+    65.00,
+    5.00,
+    35.00,
+    120.12,
+    51.48,
+    20.00
+  ];
+  const supplements = [
+    "podstawowe wsparcie dochodów",
+    "płatność redystrybucyjna",
+    "płatność dla młodych rolników",
+    "płatność do buraków cukrowych",
+    "ekoschemat międzyplony lub wsiewki",
+    "ekoschemat plan nawożenia",
+    "ekoschemat zróżnicowana struktura upraw",
+    "ekoschemat wymieszanie obornika",
+    "ekoschemat stosowanie nawozów naturalnych płynnych nierozbryzgowo",
+    "ekoschemat uproszczone systemy uprawy",
+    "ekoschemat wymieszanie słomy z glebą",
+    "ekoschemat Integrowana Produkcja Roślin",
+    "ekoschemat biologiczna ochrona upraw",
+    "ekoschemat"
+  ];
+  const supplementsValues = [
+    483.20,
+    168.79,
+    256.55,
+    1253.34,
+    432.05,
+    86.41,
+    223.18,
+    172.82,
+    259.23,
+    249.50,
+    134.60,
+    810.96,
+    297.61,
+    130.00
+];
--- script.js
+++ script.js
@@ -1,51 +1,43 @@
 document.addEventListener('DOMContentLoaded', revenuesDisplay)
 function revenuesDisplay(){
-    let revenuesInputIndex = 0;
-    let przychod = "";
-    let revenuesSum = 0;
+  let revenuesInputIndex = 0;
+  let revenue = "";
+  let supplement = "";
 
-    const revenues = [
-        "zakładany plon",
-        "plon z kontraktacji",
-        "plon pozakontraktowy",
-        "ilość wysłodków (10% s.m) na sprzedaż",
-        "cena buraków kontraktowych",
-        "cena buraków pozakontraktowych",
-        "cena wysłodków"
-      ];
-      const units = [
-        "t/ha",
-        "t/ha",
-        "t/ha",
-        "t/ha",
-        "zł/t",
-        "zł/t",
-        "zł/t"
-      ];
-      const values = [
-        70.00,
-        65.00,
-        5.00,
-        35.00,
-        120.12,
-        51.48,
-        20.00
-      ];
+  for(i=0; i<revenues.length; i++){
+      revenue += `
+                  <div class="revenue">
+                      <div class="revenue-name">${revenues[i]}</div> 
+                      <div class="value">
+                          <input type="text" inputmode="numeric" pattern="[0-9]*" value="${revenuesValues[i]}" id="${revenuesInputIndex}"></input>
+                          <div class="unit">${revenuesUnits[i]}</div>
+                      </div>
+                  </div>
+                  `;
+      revenuesInputIndex++;
+  };
 
-    for(i=0; i<revenues.length; i++){
+  for (i = 0; i < supplements.length; i++) {
+    supplement += `
+        <div class="supplement">
+    `;
 
-        przychod += `
-                    <div class="revenue">
-                        <div class="revenue-name">${revenues[i]}</div> 
-                        <div class="value">
-                            <input type="text" inputmode="numeric" pattern="[0-9]*" value="${values[i]}" id="${revenuesInputIndex}"></input>
-                            <div class="unit">${units[i]}</div>
-                        </div>
-                    </div>
-                    `;
+    if (supplements[i].startsWith('ekoschemat')) {
+        supplement += `<input type="checkbox" class="supplements-checkbox" id="${revenuesInputIndex}">`;
+    }
 
-        revenuesInputIndex++;
-    };
+    supplement += `
+            <div class="supplement-name">${supplements[i]}</div> 
+            <div class="value">
+                <input type="text" inputmode="numeric" pattern="[0-9]*" value="${supplementsValues[i]}" id="${revenuesInputIndex}">
+                <div class="unit">zł/ha</div>
+            </div>
+        </div>
+    `;
 
-    document.querySelector('#revenues-divs').innerHTML = przychod;
+    revenuesInputIndex++;
+}
+
+  document.querySelector('#revenues-divs').innerHTML = revenue;
+  document.querySelector('#supplements-divs').innerHTML = supplement;
 }
